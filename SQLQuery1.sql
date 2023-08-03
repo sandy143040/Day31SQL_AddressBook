1e825ca5323031a8e2f32ff010ff4a2de7152755
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-08-04 01:21:47 +0530

    [Sandeep] Add: created AddressBook Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -26,3 +26,15 @@
 CREATE DATABASE AddressBook_Service
 USE AddressBook_Service;
 
+--UC2: create a AddressBook Table with first and last names, address, city,state, zip, phone number and email as its attributes
+CREATE TABLE AddressBookDB(PersonID INT PRIMARY KEY IDENTITY(1,1),
+FirstName VARCHAR (30) NOT NULL,
+LastName VARCHAR(30),
+Address VARCHAR(100) NOT NULL,
+City VARCHAR (20) NOT NULL,
+State VARCHAR (20)NOT NULL,
+Zip INT NOT NULL,
+PhoneNumber BIGINT NOT NULL,
+EmailID VARCHAR(50) NOT NULL)
+
+
