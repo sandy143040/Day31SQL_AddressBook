d3458e2d36a6187cc963798a11f97859f61fd7c1
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-08-04 01:00:23 +0530

    [sandeep] Add: Created AddressBook Service DB

---
+++ SQLQuery1.sql
@@ -0,0 +1,28 @@
+--Ctrl+s
+--SQL stands for structure query language.
+--It is is used to communicate with data base.
+--It is used to CURD operations (create,update,retrive,delete)
+--SQL Commands are instructions used to perform a specific task. They are
+--DLL-Data Defination language
+      --CREATE
+	  --ALTER
+	  --DROP
+	  --TRUNCATE
+--DML-Data Manipulation Language
+     --INSERT
+	 --UPDATE
+	 --DELETE
+--DCL-Data Control Language
+      --Revoke
+	  --Grant
+--DQL-Data Query Language
+     --SELECT
+--TCL-Transation Control Language
+      --Commit
+	  --RollBack
+	  --SavePoint
+
+--UC1-Create DB
+CREATE DATABASE AddressBook_Service
+USE AddressBook_Service;
+
